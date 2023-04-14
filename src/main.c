d8eccd453a288ef1182201b579f4d3b9c83b9397
gisaki <gisaki.dev@gmail.com>
2023-04-14 21:38:53 +0900

    Added uart peripheral driver

--- src/main.c
+++ src/main.c
@@ -8,7 +8,36 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/uart.h>
 
+#include "../drivers/my_uart_peripheral/api.h"
+
 #include <string.h>
+
+
+// ----------------
+struct counter
+{
+    int strings;
+    int overflows;
+};
+
+static struct counter tracker = {0};
+
+void peripheral_callback(const struct device *dev, char *data, size_t length, bool is_string, void *user_data)
+{
+    struct counter *c = (struct counter *)user_data;
+    if (is_string)
+    {
+        printk("Recieved string \"%s\"\n", data);
+        c->strings++;
+    } else {
+        printk("Buffer full. Recieved fragment %.*s\n", length, data);
+        c->overflows++;
+    }
+    printk("Strings: %d\nOverflows: %d\n", c->strings, c->overflows);
+}
+// ----------------
+
+
 
 /* change this to any other UART peripheral if desired */
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
@@ -83,6 +112,14 @@ void main(void)
 
 	print_uart("Hello! I'm your echo bot.\r\n");
 	print_uart("Tell me something and press enter:\r\n");
+
+
+
+	// ----------------
+    const struct device *dev2 = device_get_binding(DT_LABEL(DT_NODELABEL(my_device)));
+    my_uart_set_callback(dev2, peripheral_callback, &tracker);
+	// ----------------
+
 
 	/* indefinitely wait for input from the user */
 	while (k_msgq_get(&uart_msgq, &tx_buf, K_FOREVER) == 0) {
