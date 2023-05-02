92c1854e58fdc47fc056289507a51b05984d1894
gisaki <gisaki.dev@gmail.com>
2023-05-02 15:37:19 +0900

    Added send function to USB acm.

--- src/main_cdc_acm.c
+++ src/main_cdc_acm.c
@@ -16,6 +16,7 @@
 #include <string.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/uart.h>
+#include <zephyr/sys/ring_buffer.h>
 #include <zephyr/kernel.h>
 
 #include <zephyr/usb/usb_device.h>
@@ -24,9 +25,48 @@ LOG_MODULE_REGISTER(cdc_acm, LOG_LEVEL_INF);
 
 static const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(cdc_acm_uart1));
 
+#define RING_BUF_SIZE	(128)
+struct send_buf {
+    struct ring_buf rb;
+    uint32_t buffer[RING_BUF_SIZE];
+};
+static struct send_buf sb;
+
+void send_cdc_acm(const char *data, size_t length)
+{
+	size_t wrote;
+	wrote = ring_buf_put(&(sb.rb), data, length);
+	if (wrote < length) {
+		LOG_ERR("Drop %zu bytes", length - wrote);
+	}
+	if (wrote) {
+		uart_irq_tx_enable(dev);
+	}
+}
+
 static void interrupt_handler(const struct device *dev, void *user_data)
 {
-	LOG_INF("interrupt_handler");
+	while (uart_irq_update(dev) && uart_irq_is_pending(dev)) {
+		while (uart_irq_rx_ready(dev))
+		{
+	        char c;
+			uart_fifo_read(dev, &c, 1);
+
+			send_cdc_acm(&c, 1);
+		}
+		if (uart_irq_tx_ready(dev)) {
+			uint8_t buf[16];
+			size_t wrote, len;
+			len = ring_buf_get(&(sb.rb), buf, sizeof(buf));
+			if (!len) {
+				LOG_DBG("dev %p TX buffer empty", dev);
+				uart_irq_tx_disable(dev);
+			} else {
+				wrote = uart_fifo_fill(dev, buf, len);
+				LOG_DBG("dev %p wrote len %zu", dev, wrote);
+			}
+		}
+	} // while
 }
 
 static void uart_line_set(const struct device *dev)
@@ -87,6 +127,8 @@ void main_cdc_acm(void)
 		LOG_ERR("Failed to enable USB");
 		return;
 	}
+
+	ring_buf_init(&(sb.rb), RING_BUF_SIZE, sb.buffer);
 
 	LOG_INF("Wait for DTR");
 
